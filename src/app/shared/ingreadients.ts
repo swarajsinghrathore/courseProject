158eca7d0d95fde07ab91684a034aa32c95a9369
Swaraj <swaraj731@gmail.com>
2023-06-10 00:08:33 +0530

    angular udemy project

---
+++ src/app/shared/ingreadients.ts
@@ -0,0 +1,6 @@
+export class Ingredient {
+    // public name: string;
+    // public amount: number;
+    //Another way...in this method we don't need to use this.name=name it will automatically did
+    constructor(public name:string, public amount:number){}
+}
---
+++ src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { Ingredient } from '../shared/ingreadients';
+
+@Component({
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrls: ['./shopping-list.component.css']
+})
+export class ShoppingListComponent {
+  ingreadients : Ingredient[] = [
+    new Ingredient('Apples',5),
+    new Ingredient('Tomatoes',10)
+  ];
+  constructor(){ }
+}
