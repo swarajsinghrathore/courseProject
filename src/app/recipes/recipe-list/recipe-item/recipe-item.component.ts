755c9e4d76c3ae27c9360b3a034cb88c07f49326
Swaraj <swaraj731@gmail.com>
2023-07-16 13:26:32 +0530

    showing recipe details on recipe-item click

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../../recipe.model'
 
 @Component({
@@ -8,9 +8,9 @@ import { Recipe } from '../../recipe.model'
 })
 export class RecipeItemComponent {
   @Input() recipe : Recipe;
-  recipeSelected = new EventEmitter<void>()
+  @Output() recipeSelected = new EventEmitter<void>()
   constructor(){}
   onSelected(){
-    
+    this.recipeSelected.emit();
   }
 }
